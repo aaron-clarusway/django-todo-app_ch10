16f4d133e2105922f9388c23be4851494f2dd7f5
aaron <aaron@nioyatech.com>
2022-05-25 22:58:12 +0300

    feat: general update

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django import forms
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from .models import Todo
 from .forms import TodoForm
@@ -20,23 +21,46 @@ def todo_list(request):
 
 def todo_create(request):
     form = TodoForm()
-    
+
     if request.method == "POST":
         form = TodoForm(request.POST)
-        
+
         if form.is_valid():
             form.save()
-         
-    context = {
-        "form": form
-    }   
+            return redirect("list")
+
+    context = {"form": form}
 
     return render(request, "todo/todo_add.html", context)
 
 
 def todo_update(request, id):
-    pass
+
+    todo = Todo.objects.get(id=id)
+    form = TodoForm(instance=todo)
+
+    if request.method == "POST":
+        form = TodoForm(request.POST, instance=todo)
+
+        if form.is_valid():
+            form.save()
+            return redirect("list")
+
+    context = {"todo": todo, "form": form}
+
+    return render(request, "todo/todo_update.html", context)
 
 
 def todo_delete(request, id):
-    pass
+    
+    todo = Todo.objects.get(id=id)
+    
+    if request.method == "POST":
+        todo.delete()
+        return redirect("list")
+    
+    context = {
+        "todo": todo
+    }
+    
+    return render(request, "todo/todo_delete.html", context)
